3b56cfe94e02a276043274aefa6af0c2bc728206
yarmolovichalex <yarmolovichalex@gmail.com>
2017-02-06 14:15:23 +0300

    add Artist page

---
+++ MusicStoreEF/Controllers/ArtistController.cs
@@ -0,0 +1,25 @@
+﻿using MusicStoreEF.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MusicStoreEF.Controllers
+{
+    public class ArtistController : Controller
+    {
+        private readonly MusicStoreDbContext _context;
+
+        public ArtistController()
+        {
+            _context = new MusicStoreDbContext();
+        }
+
+        public ActionResult Index(int id)
+        {
+            var artist = _context.Artists.SingleOrDefault(a => a.Id == id);
+            if (artist == null)
+                return HttpNotFound();
+            
+            return View(artist);
+        }
+    }
+}
