caa68f672948841d4ebf122c9ff4323dd837b133
yarmolovichalex <yarmolovichalex@gmail.com>
2017-02-27 12:38:49 +0300

    genres dropdown

---
+++ MusicStoreEF/Controllers/GenreController.cs
@@ -0,0 +1,41 @@
+﻿using MusicStoreEF.Models;
+using MusicStoreEF.ViewModels;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MusicStoreEF.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly MusicStoreDbContext _context;
+
+        public GenreController()
+        {
+            _context = new MusicStoreDbContext();
+        }
+
+        public PartialViewResult GetGenres()
+        {
+            var model = _context.Genres.ToList();
+            return PartialView("_Genres", model);
+        }
+
+        public ActionResult Index(byte genreId)
+        {
+            var genre = _context.Genres.Single(g => g.Id == genreId);
+            var releases = _context.Releases
+                .Include(r => r.Artists)
+                .Where(r => r.GenreId == genreId)
+                .ToList();
+
+            var model = new GenreViewModel
+            {
+                Genre = genre.Name,
+                Releases = releases
+            };
+
+            return View(model);
+        }
+    }
+}
